f98db340ab98ceb8590f82f135d5a04870341fa8
John <likisongproduct@gmail.com>
2021-03-18 18:02:21 -0700

    working bigboi ability

--- src/powerup.cpp
+++ src/powerup.cpp
@@ -16,11 +16,18 @@ void PowerupSystem::handle_powerups()
 {
 	// Loop over all collisions detected by the physics system
 	auto& registry = ECS::registry<PowerupSystem::Powerup>;
-	for (auto& powerup : ECS::registry<PowerupSystem::Powerup>.components)
+	for (auto& entity : ECS::registry<PowerupSystem::Powerup>.entities)
 	{
+		PowerupSystem::Powerup& powerup = ECS::registry<PowerupSystem::Powerup>.get(entity);
 		std::cout << powerup.power << std::endl;
 		// create random powerup
-
+		if (powerup.power == "bigboi")
+		{
+			std::cout << powerup.power << std::endl;
+			powerup.duration = 1;
+			Motion& motion = ECS::registry<Motion>.get(entity);
+			motion.scale *= vec2(2.f, 2.f);
+		}
 
 	}
 	// Remove all collisions from this simulation step
@@ -29,8 +36,8 @@ void PowerupSystem::handle_powerups()
 
 void PowerupSystem::Powerup::createPowerup(ECS::Entity& entity)
 {
-	PowerupSystem::Powerup powerup = ECS::registry<PowerupSystem::Powerup>.emplace(entity);
+	PowerupSystem::Powerup& powerup = ECS::registry<PowerupSystem::Powerup>.emplace(entity);
 	powerup.owner = entity;
-	powerup.power = "randomPowerup";
+	powerup.power = "bigboi";
 	powerup.duration = 1;
 }
